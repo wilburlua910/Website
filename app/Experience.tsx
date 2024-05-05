2409276890464ac339947499f565ccefd1b783b3
wilburlua910 <wilburlua910@outlook.com>
2024-05-05 21:59:10 +0800

    Add Experience section card template

--- app/Experience.tsx
+++ app/Experience.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import WorkExperienceCard from "./components/WorkExperienceCard";
 
 const Experience = () => {
   return (
@@ -12,6 +14,7 @@ const Experience = () => {
       <div className="flex-1 container mx-auto grid grid-cols-2 gap-5 mb-10">
         <div className="w-full h-full border-2 rounded bg-gray-800">
           <p className="flex justify-center mt-10 font-bold text-xl text-white"></p>
+          <WorkExperienceCard></WorkExperienceCard>
         </div>
 
         <div className="container flex flex-col justify-between">
@@ -27,9 +30,45 @@ const Experience = () => {
           </div>
 
           <div className="w-full h-1/3 border-2 rounded bg-gray-800">
-            <p className="flex justify-center mt-10 font-bold text-xl text-white">
+            <p className="flex justify-center mt-5 mb-5 font-bold text-xl text-white">
               Certifications (I'm working on it!)
             </p>
+
+            <div className="flex justify-evenly">
+              <a
+                target="_blank"
+                href="https://www.credly.com/badges/159d9efc-7317-4850-924b-fe0e2badb188/public_url"
+              >
+                <Image
+                  src={"/assets/badges/SA.png"}
+                  height={100}
+                  width={100}
+                  alt="Solution Architect"
+                ></Image>
+              </a>
+              <a
+                target="_blank"
+                href="https://www.credly.com/badges/8d4133e8-0b4d-45f7-aedd-da7cd9b21f72/public_url"
+              >
+                <Image
+                  src={"/assets/badges/Developer.png"}
+                  height={100}
+                  width={100}
+                  alt="Solution Architect"
+                ></Image>
+              </a>
+              <a
+                target="_blank"
+                href="https://www.credly.com/badges/8b380ab5-d2d8-46e9-a27b-09641c722e58/public_url"
+              >
+                <Image
+                  src={"/assets/badges/SysOps.png"}
+                  height={100}
+                  width={100}
+                  alt="Solution Architect"
+                ></Image>
+              </a>
+            </div>
           </div>
         </div>
       </div>
---
+++ app/components/WorkExperienceCard.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Image from "next/image";
+
+const WorkExperienceCard = () => {
+  return (
+    <div className="flex flex-col ml-10">
+      {/* This is one row of card*/}
+      <div className="flex flex-row">
+        <div className="flex shrink-0 h-12 w-12 bg-white rounded-full mr-10"></div>
+
+        <div className="flex flex-col">
+          <p className="font-bold text-base text-white">DevSecOps Engineer</p>
+
+          <p className="font-semibold text-sm text-white">IBM Singapore</p>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+        </div>
+      </div>
+
+      <div className="flex flex-row mt-10">
+        <div className="flex shrink-0 h-12 w-12 bg-white rounded-full mr-10"></div>
+
+        <div className="flex flex-col">
+          <p className="font-bold text-base text-white">Software Engineer</p>
+
+          <p className="font-semibold text-sm text-white">CARRO</p>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+        </div>
+      </div>
+
+      <div className="flex flex-row mt-10">
+        <div className="flex shrink-0 h-12 w-12 bg-white rounded-full mr-10"></div>
+
+        <div className="flex flex-col">
+          <p className="font-bold text-base text-white">Software Engineer</p>
+
+          <p className="font-semibold text-sm text-white">
+            Univers (Envision Digital)
+          </p>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkExperienceCard;
