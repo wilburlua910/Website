ce5f7d370cfeb9c4cf823b16bf854ceb94637ce9
wilburlua910 <wilburlua910@outlook.com>
2024-05-06 22:13:48 +0800

    Add Experience section card template

--- app/components/WorkExperienceCard.tsx
+++ app/components/WorkExperienceCard.tsx
@@ -5,7 +5,7 @@ const WorkExperienceCard = () => {
   return (
     <div className="flex flex-col ml-10">
       {/* This is one row of card*/}
-      <div className="flex flex-row">
+      <div className="flex flex-row mr-5">
         <div className="flex shrink-0 h-12 w-12 bg-white rounded-full mr-10"></div>
 
         <div className="flex flex-col">
@@ -23,7 +23,7 @@ const WorkExperienceCard = () => {
         </div>
       </div>
 
-      <div className="flex flex-row mt-10">
+      <div className="flex flex-row mt-10 mr-5">
         <div className="flex shrink-0 h-12 w-12 bg-white rounded-full mr-10"></div>
 
         <div className="flex flex-col">
@@ -41,7 +41,27 @@ const WorkExperienceCard = () => {
         </div>
       </div>
 
-      <div className="flex flex-row mt-10">
+      <div className="flex flex-row mt-10 mr-5">
+        <div className="flex shrink-0 h-12 w-12 bg-white rounded-full mr-10"></div>
+
+        <div className="flex flex-col">
+          <p className="font-bold text-base text-white">Software Engineer</p>
+
+          <p className="font-semibold text-sm text-white">
+            Univers (Envision Digital)
+          </p>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+          <li className="font-light text-sm text-white">
+            Deployed scalable and secure infrastructure for multiple large-scale
+            projects with AWS Cloud
+          </li>
+        </div>
+      </div>
+
+      <div className="flex flex-row mt-10 mr-5">
         <div className="flex shrink-0 h-12 w-12 bg-white rounded-full mr-10"></div>
 
         <div className="flex flex-col">
